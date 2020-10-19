12031570cdf10e21a8acdadb5b2f6194dafba4de
AdamHGDD <ash5572@rit.edu>
2020-10-18 21:19:44 -0400

    I hate mixed tabs and spaces

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -8,55 +8,55 @@ const { Domo } = models;
 const makerPage = (req, res) => {
 	// Find the domo for the active user
 	Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-  	if(err) {
-  		console.log(err);
-  		return res.status(400).json({ error: 'An error occurred' });
-  	}
+		if(err) {
+			console.log(err);
+			return res.status(400).json({ error: 'An error occurred' });
+		}
 
-  	// Send the data to the page
-  	return res.render('app', {domos: docs});
-  })
+		// Send the data to the page
+		return res.render('app', {domos: docs});
+	})
 };
 
 // Make a domo
 const makeDomo = (req, res) => {
-  // Check for valid data
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'RAWR! Both name and age are required' });
-  }
+	// Check for valid data
+	if (!req.body.name || !req.body.age) {
+		return res.status(400).json({ error: 'RAWR! Both name and age are required' });
+	}
 
-  console.log("about to see req.session");
-  console.dir(req.session);
-  console.log("done seeing req.session");
+	console.log("about to see req.session");
+	console.dir(req.session);
+	console.log("done seeing req.session");
 
-  // Create domo js object
-  const domoData = {
-    name: req.body.name,
-    age: req.body.age,
-    owner: req.session.account._id,
-  };
+	// Create domo js object
+	const domoData = {
+		name: req.body.name,
+		age: req.body.age,
+		owner: req.session.account._id,
+	};
 
-  // Make a new domo model from the js object created above
-  const newDomo = new Domo.DomoModel(domoData);
+	// Make a new domo model from the js object created above
+	const newDomo = new Domo.DomoModel(domoData);
 
-  // Save new domo
-  const domoPromise = newDomo.save();
+	// Save new domo
+	const domoPromise = newDomo.save();
 
-  // Finish
-  domoPromise.then(() => res.json({ redirect: '/maker' }));
+	// Finish
+	domoPromise.then(() => res.json({ redirect: '/maker' }));
 
-  // Catch errors
-  domoPromise.catch((err) => {
-    console.log(err);
-    if (err.code === 11000) {
-      return res.status(400).json({ error: 'Domo already exists.' });
-    }
+	// Catch errors
+	domoPromise.catch((err) => {
+		console.log(err);
+		if (err.code === 11000) {
+			return res.status(400).json({ error: 'Domo already exists.' });
+		}
 
-    return res.status(400).json({ error: 'An error occurred' });
-  });
+		return res.status(400).json({ error: 'An error occurred' });
+	});
 
-  // Complete and return the saved object
-  return domoPromise;
+	// Complete and return the saved object
+	return domoPromise;
 };
 
 // Exports
