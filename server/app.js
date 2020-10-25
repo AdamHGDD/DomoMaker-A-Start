dbcc8beefed042b6d84332af553e1466239294d4
AdamHGDD <ash5572@rit.edu>
2020-10-25 17:05:21 -0400

    Redis

--- server/app.js
+++ server/app.js
@@ -8,9 +8,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
 
 // Get port
-const port = process.env.PORT || process.env.NODE_PORT || 3001;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // Get database
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -29,6 +32,27 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
+// Set up redis
+let redisURL = {
+	// My hostname and port for local use
+	hostname: 'redis-10684.c8.us-east-1-4.ec2.cloud.redislabs.com',
+	port: '10684',
+};
+
+// My password for local use
+let redisPass = '47C1HMcAHQLC2OvZ5rHHl4sh9hXtlCZG'
+// Get heroku info if not local
+if(process.env.REDISCLOUD_URL) {
+	redisURL = url.parse(process.env.REDISCLOUD_URL);
+	[, redisPass] = redisURL.auth.split(':');
+}
+// Start connection to redis database
+let redisClient = redis.createClient({
+	host: redisURL.hostname,
+	port: redisURL.port,
+	password: redisPass,
+});
+
 // Pull in our routes
 const router = require('./router.js');
 
@@ -42,9 +66,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+  	client: redisClient,
+  }),
   secret: 'Bippity Boppity',
   resave: true,
   saveUnitialized: true,
+  cookie: {
+  	httpOnly: true,
+  }
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
